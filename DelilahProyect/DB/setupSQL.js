0a3f273bfe8dbe20e76305a016a25d77c332eefe
Moises-dev08 <57867993+Moises-dev08@users.noreply.github.com>
2020-06-19 11:19:02 -0300

    Update setupSQL.js

--- DelilahProyect/DB/setupSQL.js
+++ DelilahProyect/DB/setupSQL.js
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Data base: `delilah_resto_proyect`
+-- Data base: `delilah_resto_project`
 --
 
 -- --------------------------------------------------------
